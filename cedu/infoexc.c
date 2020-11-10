369324c5be67ec6a67fdaf8b9e70c54cebbbfdaf
SanttuA98 <asikainensanttu98@gmail.com>
2020-11-10 10:27:28 +0200

    Create infoexc.c

---
+++ cedu/infoexc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main(void)
+  {
+ float size[]="44",balance[]="34.43";
+ char fname[13]="Charley",sname[13]="Brown",gender[]="M",address[30]="Willowstreet",number[14]="050234243242";
+ int age[]="21";
+ 
+ printf("Name:%c\n",sname[13],fname[13]);
+ printf("Gender:%c\n",gender);
+ printf("give age\n",age);
+ printf("give shoe size\n",size);
+ printf("give bank account balance\n",balance);
+ printf("give address\n",address[30]);
+ printf("give phone number\n",number[14]);
+
+ printf("Name:%s %s \n Gender:%c\n Address:%s\n Phone:%s\n Age:%d\n Bank balance:%.2f\n Shoe size:%.0f",fname,sname,gender,address,number,age,balance,si$
+
+ return 0;
+ }
+
