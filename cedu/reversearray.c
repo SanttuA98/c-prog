ef4e3656b82b4fadfbc18502d5573c36d2c83d10
SanttuA98 <asikainensanttu98@gmail.com>
2020-11-10 11:00:32 +0200

    Create reversearray.c

---
+++ cedu/reversearray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(void)
+{
+int a;
+int i,array[15];
+for(i=0;i<15;i++){
+a=rand()%80 -40;
+array[i]=a;
+}
+printf("Array of size 15 filled with random numbers (between -40 ... 40) is:\n");
+for(i=0;i<15;i++)
+printf("%d,",array[i]);
+
+printf("\nSame array in reverse order is: \n");
+for(i=14;i>=0;i--) 
+printf("%d,",array[i]);
+
+return 0;
+}
